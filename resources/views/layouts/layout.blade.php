fde6b8b09282539a99ca22ad4553cd9a9d3ff07a
Namiki-Niwa <109219684+Namiki-Niwa@users.noreply.github.com>
2024-04-17 03:11:13 +0800

    header.bladeの作成&layoutの作成

---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Final Project</title>
+  {{-- 独自cssの読み込み --}}
+  <link rel="stylesheet" href="{{ asset('/css/reset.css')  }}" >
+  <link rel="stylesheet" href="{{ asset('/css/header-blade.css')  }}" >
+
+  {{-- Bootstrap5のCDN読み込み記述 --}}
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+
+</head>
+<body>
+  @include('layouts.header')
+  <div>
+    @yield('content')
+  </div>
+
+
+  {{-- Bootstrap、JSのCDN読み込み --}}
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</script>
+</body>
+</html>
