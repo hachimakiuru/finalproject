9cbaaeba4056618353c9258f6bae267751c9eb68
Namiki-Niwa <109219684+Namiki-Niwa@users.noreply.github.com>
2024-04-17 20:55:11 +0800

    ヘッダーcss変更

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,13 +1,14 @@
 <header class="header">
   <div class="inner">
     <div class="header-wrapper">
+      <h1 class="header-logo">BSPK Shinjuku</h1>
       <ul class="header-nav">
         <li><a href="#">Home</li></a>
         <li><a href="#">Food</li></a>
         <li><a href="#">Activity</li></a>
         <li><a href="#">Chat Message</li></a>
       </ul>
-      <p class="user-name"><a href="#">丹羽 南樹</a></p>
     </div>
+    <p class="user-name"><a href="#">丹羽 南樹</a></p>
   </div>
 </header>
