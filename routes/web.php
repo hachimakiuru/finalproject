9843d4e3deec8ea49deb3d61094bf6b709ef8829
mikuoyamada <miku44xx@gmail.com>
2024-04-21 21:37:51 +0800

    いいねっていいね

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,11 @@
 
 
 
-use Illuminate\Support\Facades\Route;
+
 use App\Http\Controllers\RestaurantPostController;
 
 
-/*
-|-------------------------------------------------------------------------- 
-| Web Routes
+
 
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
@@ -27,15 +25,7 @@ use App\Http\Controllers\ExperienceController;
 use App\Http\Controllers\LikeController;
 
 
-/*
-|--------------------------------------------------------------------------
-- Web Routes
 
-|--------------------------------------------------------------------------
-- | Here is where you can register web routes for your application. These
-- routes are loaded by the RouteServiceProvider and all of them will
-- be assigned to the "web" middleware group. Make something great!
-- */
 
 
 Route::get('/', function () {
