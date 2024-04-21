f2085a3cc54437a587cb6b361a0ed7aaaaeb5f7d
Namiki-Niwa <109219684+Namiki-Niwa@users.noreply.github.com>
2024-04-21 14:14:24 +0800

    ルートにコメント追加

--- routes/web.php
+++ routes/web.php
@@ -33,19 +33,14 @@ Route::get('/layout', function () {
 
 // -----------------------------------------------
 
+// ログイン・ログアウト・レジスター・
 Route::get('/register', [AuthController::class, 'register'])->name('register');
 Route::post('/register', [AuthController::class, 'store']);
 Route::get('/login', [AuthController::class, 'login'])->name('login');
 Route::post('/login', [AuthController::class, 'authenticate']);
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
-
-
-// Route::middleware(['auth', 'can:admin'])->prefix('/admin')->as('admin.')->group(function () {
-//     Route::get('/', [AdminDashboardController::class, 'index'])->name('dashboard');
-// });
-
 Route::get('/users', [AdminDashboardController::class, 'index'])->name('admin.dashboard')->middleware(['auth', 'can:admin']);
 
-
 Route::delete('/admin/dashboard/{user}', [UserController::class, 'destroy'])->name('user.destroy');
+// --------------------------------------------------------------
