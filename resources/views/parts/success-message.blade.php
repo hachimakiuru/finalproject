1e349762dffe40850892ab9b942a3a561eb6a05f
Namiki-Niwa <109219684+Namiki-Niwa@users.noreply.github.com>
2024-04-23 18:12:51 +0800

    cssフォルダ構成の修正
    ・共有cssファイルをPublic > Globalフォルダ内に移動し、パスも変更しています。

--- resources/views/parts/success-message.blade.php
+++ resources/views/parts/success-message.blade.php
@@ -1,7 +1,8 @@
 @if (session()->has('success'))
-
-<div class="alert alert-success alert-dismissible fade show" role="alert">
-  {{ session('success') }}
-  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+<div class="success-message">
+  <div class="alert alert-success alert-dismissible fade show" role="alert">
+    {{ session('success') }}
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+  </div>
 </div>
 @endif
