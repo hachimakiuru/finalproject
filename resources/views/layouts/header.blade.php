6618374050b1fe9ed52e69dc23da4f83f9c1ae9c
Namiki-Niwa <109219684+Namiki-Niwa@users.noreply.github.com>
2024-04-24 11:40:20 +0800

    header-nav日本語に変更

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -2,15 +2,15 @@
   <div class="inner">
       @auth
       <ul class="header-nav">
-        <li><a href="{{ route('welcome') }}">Home</a></li>
-        <li><a href="{{ route('restaurants.index') }}">Food</a></li>
-        <li><a href="{{ route('activity.dashboard') }}">Activity</a></li>
-        <li><a href="#">Chat Message</a></li>
+        <li><a href="{{ route('welcome') }}">ホーム</a></li>
+        <li><a href="{{ route('restaurants.index') }}">グルメ</a></li>
+        <li><a href="{{ route('activity.dashboard') }}">アクティビティ</a></li>
+        <li><a href="#">チャット</a></li>
         @auth
         @if(auth()->user()->role_id === 1)
-        <li><a href="{{ route('admin.dashboard') }}">Admin Page</a></li>
-        <li><a href="{{ route('rbooking.index') }}">Restaurant Reservation List</a></li>
-        <li><a href="{{ route('newsBookings.index') }}">Activity Reservation List</a></li>
+        <li><a href="{{ route('admin.dashboard') }}">管理者画面</a></li>
+        <li><a href="{{ route('rbooking.index') }}">レストラン予約一覧</a></li>
+        <li><a href="{{ route('newsBookings.index') }}">アクティビティ予約一覧</a></li>
         @endif
         @endauth
       </ul>
