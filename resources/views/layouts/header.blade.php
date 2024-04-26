822059096bd70c1a54901c6085ffe551232d01e0
Namiki-Niwa <109219684+Namiki-Niwa@users.noreply.github.com>
2024-04-26 23:26:54 +0800

    header修正
    ヘッダーのnavを変更しました。

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -2,15 +2,16 @@
   <div class="inner">
       @auth
       <ul class="header-nav">
-        <li><a href="{{ route('welcome') }}">ホーム</a></li>
-        <li><a href="{{ route('restaurants.index') }}">グルメ</a></li>
-        <li><a href="{{ route('activity.dashboard') }}">アクティビティ</a></li>
-        <li><a href="#">チャット</a></li>
+        <li><a href="{{ route('welcome') }}">Home</a></li>
+        <li><a href="{{ route('restaurants.index') }}">Gourmet</a></li>
+        <li><a href="{{ route('activity.dashboard') }}">Activity</a></li>
+        <li><a href="#">Chat</a></li>
+        <li><a href="#">Calender</a></li>
         @auth
         @if(auth()->user()->role_id === 1)
-        <li><a href="{{ route('admin.dashboard') }}">管理者画面</a></li>
-        <li><a href="{{ route('rbooking.index') }}">レストラン予約一覧</a></li>
-        <li><a href="{{ route('newsBookings.index') }}">アクティビティ予約一覧</a></li>
+        <li><a href="{{ route('admin.dashboard') }}">Admin Page</a></li>
+        <li><a href="{{ route('rbooking.index') }}">Restaurant Booking List</a></li>
+        <li><a href="{{ route('newsBookings.index') }}">Activity Booking List</a></li>
         @endif
         @endauth
       </ul>
@@ -19,7 +20,7 @@
         <li>
           <form action="{{ route('logout') }}" method="post">
             @csrf
-            <button class="btn btn-danger" type="submit">ログアウト</button>
+            <button class="btn btn-danger" type="submit">Logout</button>
           </form>
         </li>
       </ul>
