ef2b85c8f4283672e3c89a638504b226edba0e8e
M-riko7 <141759642+M-riko7@users.noreply.github.com>
2024-04-27 16:30:59 +0800

    wekcomページのCSS終わりました

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,28 +5,35 @@
 @endpush
 
 @include('parts.success-message')
-<div class="welcome__container">
-  <div>
-    <a class="welcome__box" href="{{ route('activity.dashboard') }}">
-      <div class="welcome__box-img">
-        <img src="{{ asset('/img/welcome-food.png') }}" alt="">
-        <span class="welcome__box-title">Activity</span>
-      </div>
-      <div class="welcome__box-text">
-        <p class="welcome__text">Go and check our posts</p>
-      </div>
-    </a>
-  </div>
-  <div>
-    <a class="welcome__box" href="{{ route('restaurants.index') }}">
-      <div class="welcome__box-img">
-        <img src="{{ asset('/img/welcome-food.png') }}" alt="">
-        <span class="welcome__box-title">Gourmet</span>
-      </div>
-      <div class="welcome__box-text">
-        <p class="welcome__text">Go and check our posts</p>
-      </div>
-    </a>
+
+<div class="body">
+  <div class="welcome_container">
+    <div class="h1-text">
+      <h1>What's on your mind?</h1>
+    </div>
+    <div class="welcome_box box1"> 
+      <a href="{{ route('activity.dashboard') }}"> 
+        <div class="welcome_box-img">
+          <img src="{{ asset('/img/activity.jpg') }}" alt="">
+          <span class="welcome_box-title">Activity</span>
+        </div>
+        <div class="welcome_box-text">
+          <p class="welcome_text">↑ Go and check our posts ↑</p>
+        </div>
+      </a>
+    </div> 
+
+    <div class="welcome_box box2"> 
+      <a href="{{ route('restaurants.index') }}"> 
+        <div class="welcome_box-img">
+          <img src="{{ asset('/img/gourmet.jpg') }}" alt="">
+          <span class="welcome_box-title">Gourmet</span>
+        </div>
+        <div class="welcome_box-text">
+          <p class="welcome_text">↑ Go and check our posts ↑</p>
+        </div>
+      </a>
+    </div> 
   </div>
 </div>
 
