ddec3cf3376699b332d65707fa084e9d95720dee
Namiki-Niwa <109219684+Namiki-Niwa@users.noreply.github.com>
2024-05-01 21:20:20 +0800

    Auth表示の修正
    レジスターとログインの表示を日本語に、メールとIDOKの表示に変更

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -18,7 +18,7 @@ class UsersTableSeeder extends Seeder
                 'name' => 'Admin User',
                 'email' => 'admin@test.com',
                 'password' => bcrypt('12345678'), // You may use Hash::make() instead of bcrypt() depending on your Laravel version
-                'room_number' => '100',
+                'room_number' => '1',
                 'role_id' => 1, // Assuming 1 is the ID of the 'Admin' role
             ],
             [
