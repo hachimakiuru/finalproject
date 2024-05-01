96a22a53af70c38cbb560c475421f31a83a66613
Namiki-Niwa <109219684+Namiki-Niwa@users.noreply.github.com>
2024-05-02 00:31:39 +0800

    ヘッダーActive Linkの実装

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -2,18 +2,18 @@
   <div class="inner">
       @auth
       <ul class="header-nav">
-        <li><a href="{{ route('welcome') }}">Home</a></li>
-        <li><a href="{{ route('restaurants.index') }}">Gourmet</a></li>
-        <li><a href="{{ route('activity.dashboard') }}">Activity</a></li>
+        <li><a class="{{ Route::is('welcome') ? 'text-dark bg-white rounded py-1 px-2' : ' '}}" href="{{ route('welcome') }}">Home</a></li>
+        <li><a class="{{ Route::is('restaurants.index') ? 'text-dark bg-white rounded py-1 px-2' : ' '}}" href="{{ route('restaurants.index') }}">Gourmet</a></li>
+        <li><a class="{{ Route::is('activity.dashboard') ? 'text-dark bg-white rounded py-1 px-2' : ' '}}" href="{{ route('activity.dashboard') }}">Activity</a></li>
         {{-- <li><a href="#">Chat</a></li> --}}
-        <li><a href="{{ route('chatify') }}">Chat</a></li>
+        <li><a class="{{ Route::is('chatify') ? 'text-dark bg-white rounded py-1 px-2' : ' '}}" href="{{ route('chatify') }}">Chat</a></li>
         <li><a href="#">Calender</a></li>
-        <li><a href="{{ route('experience.index') }}">Posting</a></li>
+        <li><a class="{{ Route::is('experience.index') ? 'text-dark bg-white rounded py-1 px-2' : ' '}}" href="{{ route('experience.index') }}">Posting</a></li>
         @auth
         @if(auth()->user()->role_id === 1)
-        <li><a href="{{ route('admin.dashboard') }}">Admin Page</a></li>
-        <li><a href="{{ route('rbooking.index') }}">Restaurant Booking List</a></li>
-        <li><a href="{{ route('newsBookings.index') }}">Activity Booking List</a></li>
+        <li><a class="{{ Route::is('admin.dashboard') ? 'text-dark bg-white rounded py-1 px-2' : ' '}}" href="{{ route('admin.dashboard') }}">Admin Page</a></li>
+        <li><a class="{{ Route::is('rbooking.index') ? 'text-dark bg-white rounded py-1 px-2' : ' '}}" href="{{ route('rbooking.index') }}">Restaurant Booking List</a></li>
+        <li><a class="{{ Route::is('newsBookings.index') ? 'text-dark bg-white rounded py-1 px-2' : ' '}}" href="{{ route('newsBookings.index') }}">Activity Booking List</a></li>
         @endif
         @endauth
       </ul>
