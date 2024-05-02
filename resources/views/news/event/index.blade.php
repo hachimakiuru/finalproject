caa5e0a33d1cbcb7d7e3381d4fc121e1007689e1
M-riko7 <141759642+M-riko7@users.noreply.github.com>
2024-05-02 20:42:57 +0800

    英語の翻訳

--- resources/views/news/event/index.blade.php
+++ resources/views/news/event/index.blade.php
@@ -151,7 +151,7 @@
     <div class="modal-dialog modal-dialog-centered" style="max-width: 90%;">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">{{ $newsTimeLine->title }}</h5>
+                <h5 class="modal-title" id="exampleModalLabel">Details & Reservation Form</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
         
