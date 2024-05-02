4fc76a8ed346358f3f4051d879a5472b240e15c9
Namiki-Niwa <109219684+Namiki-Niwa@users.noreply.github.com>
2024-05-02 10:03:47 +0800

    カレンダースペル修正

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -6,7 +6,7 @@
         <li><a class="{{ Route::is('chatify') ? 'text-dark bg-white rounded py-1 px-2' : ' '}}" href="{{ route('chatify') }}">Chat</a></li>
         <li><a class="{{ Route::is('restaurants.index') ? 'text-dark bg-white rounded py-1 px-2' : ' '}}" href="{{ route('restaurants.index') }}">Food & Beverage</a></li>
         <li><a class="{{ Route::is('activity.dashboard') ? 'text-dark bg-white rounded py-1 px-2' : ' '}}" href="{{ route('activity.dashboard') }}">Activity</a></li>
-        <li><a class="{{ Route::is('calender.view') ? 'text-dark bg-white rounded py-1 px-2' : ' '}}" href="{{ route('calendar.view') }}">Calendar</a></li>
+        <li><a class="{{ Route::is('calendar.view') ? 'text-dark bg-white rounded py-1 px-2' : ' '}}" href="{{ route('calendar.view') }}">Calendar</a></li>
         <li><a class="{{ Route::is('experience.index') ? 'text-dark bg-white rounded py-1 px-2' : ' '}}" href="{{ route('experience.index') }}">Memory</a></li>
         @auth
         @if(auth()->user()->role_id === 1)
